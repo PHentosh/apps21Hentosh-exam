596ffe01ac14fdc8f2b5e6cfb2d34bd33451a740
PHentosh <petro.hentosh@gmail.com>
2022-01-10 14:35:39 +0200

    Init

---
+++ src/main/java/domain/Student.java
@@ -0,0 +1,51 @@
+package domain;
+
+import json.*;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Andrii_Rodionov on 1/3/2017.
+ */
+public class Student extends BasicStudent {
+
+    protected Tuple<String, Integer>[] exams;
+
+    public Student(String name, String surname, Integer year, Tuple<String, Integer>... exams) {
+        super(name, surname, year);
+        this.exams = exams;
+    }
+
+    @Override
+    public JsonObject toJsonObject() {
+
+        JsonObject[] jarr = new JsonObject[exams.length];
+        int i = 0;
+        for(Tuple val: exams){
+            JsonObject jpb;
+            if ((Integer)val.value >= 3){
+                jpb = new JsonObject(
+                        new JsonPair("course", new JsonString((String)val.key)),
+                        new JsonPair("mark", new JsonNumber((Integer)val.value)),
+                        new JsonPair("passed", new JsonBoolean(true))
+                );
+            } else {
+                jpb = new JsonObject(
+                        new JsonPair("course", new JsonString((String)val.key)),
+                        new JsonPair("mark", new JsonNumber((Integer)val.value)),
+                        new JsonPair("passed", new JsonBoolean(false))
+                );
+            }
+            jarr[i] = jpb;
+            i++;
+        }
+        JsonObject jsonObject = new JsonObject(
+                new JsonPair("name", new JsonString(name)),
+                new JsonPair("surname", new JsonString(surname)),
+                new JsonPair("year", new JsonNumber(year)),
+                new JsonPair("exams", new JsonArray(jarr))
+        );
+
+        return jsonObject;
+    }
+}
---
+++ src/main/java/json/JsonObject.java
@@ -0,0 +1,68 @@
+package json;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by Andrii_Rodionov on 1/3/2017.
+ */
+public class JsonObject extends Json {
+    private ArrayList<JsonPair> lst;
+
+    public JsonObject(JsonPair... jsonPairs) {
+        lst = new ArrayList<>();
+        for (JsonPair v: jsonPairs){
+            if (find(v.key) != null){
+                lst.remove(new JsonPair(v.key, find(v.key)));
+            }
+            lst.add(v);
+        }
+    }
+
+    @Override
+    public String toJson() {
+        StringBuilder ret = new StringBuilder("{");
+        Iterator<JsonPair> jsonIterator = lst.iterator();
+        while (jsonIterator.hasNext()) {
+            JsonPair val = jsonIterator.next();
+            ret.append("'").append(val.key).append("'");
+            ret.append(": ");
+
+            ret.append(val.value.toJson());
+
+            if (jsonIterator.hasNext()) {
+                ret.append(", ");
+            }
+        }
+        ret.append("}");
+        return ret.toString();
+    }
+
+    public void add(JsonPair jsonPair) {
+        if (find(jsonPair.key) != null){
+            lst.remove(new JsonPair(jsonPair.key, find(jsonPair.key)));
+        }
+        lst.add(jsonPair);
+    }
+
+    public Json find(String name) {
+        for (JsonPair val: lst){
+            if (val.key.equals(name)){
+                return val.value;
+            }
+        }
+        return null;
+    }
+
+    public JsonObject projection(String... names) {
+        JsonObject jsonob = new JsonObject();
+        for (JsonPair val: lst){
+            for (String st: names){
+                if (val.key.equals(st)){
+                    jsonob.add(val);
+                }
+            }
+        }
+        return jsonob;
+    }
+}
